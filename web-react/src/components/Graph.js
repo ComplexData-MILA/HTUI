1bf5ce96362e783c528f91f25640d358de0ddca6
Amanda Li <xinyue_amanda_li@yahoo.com>
2021-07-26 11:22:48 -0500

    one useState, smooth typing in filter

--- web-react/src/components/Graph.js
+++ web-react/src/components/Graph.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 // import React, { useEffect } from 'react'
-import { useQuery, gql } from '@apollo/client'
+import { useQuery, useLazyQuery, gql } from '@apollo/client'
 // import Graphin from '@antv/graphin'
 import Graphin, { Utils } from '@antv/graphin'
 import '@antv/graphin/dist/index.css'
@@ -44,86 +44,106 @@ const GET_GRAPH = gql`
 function GraphDisplay(props) {
   // declare useState hooks
   const { classes } = props
-  const [filterState, setFilterState] = useState({ nameFilter: '' })
+  // const [filterState, setFilterState] = useState({ nameFilter: '' })
   const [graphState, setGraphState] = useState({
     graphStateData: Utils.mock(13).circle().graphin(),
   })
 
-  const { data: allPeople } = useQuery(GET_PERSON, {
-    variables: { name_CONTAINS: '' },
-  })
-
-  const getFilter = () => {
-    console.log('in getFilter()')
-    return filterState.nameFilter.length > 0
-      ? {
-          OR: [
-            { name_CONTAINS: filterState.nameFilter },
-            { surname_CONTAINS: filterState.nameFilter },
-          ],
-        }
-      : {}
-  }
-
-  //graphql cyphers
-  var name = 'Amanda',
-    surname = 'Alexander'
-
-  console.log('getting personData')
   const {
-    loading: loadingPerson,
-    data: personData,
-    error: errorPerson,
-  } = useQuery(GET_PERSON, { variables: { filter: getFilter() } })
+    error: errorAll,
+    data: allPeople,
+    loading: loadingAll,
+  } = useQuery(GET_PERSON, { variables: { filter: { name_CONTAINS: '' } } })
 
-  if (!loadingPerson && !errorPerson && personData && personData.people[0]) {
-    var currentPerson = (({ name, surname }) => ({ name, surname }))(
-      personData.people[0]
-    )
-    name = currentPerson.name
-    surname = currentPerson.surname
-    // { name, surname } = {...dataPerson.people[0]}
-  }
-
-  console.log('getting file data')
-
-  const { loading, data, error } = useQuery(GET_GRAPH, {
-    variables: {
-      nameInput: name,
-      surnameInput: surname,
-    },
-  })
+  const [getGraph, { loading, data, error }] = useLazyQuery(GET_GRAPH)
+  if (errorAll) return <p>ERROR</p>
+  if (loadingAll) return <p>LOADING</p>
   if (error) return <p>Error</p>
   if (loading) return <p>Loading</p>
 
-  //manipulate json data for Graphin requirements
-  let graphData = JSON.parse(data.response)
-  graphData.nodes.forEach(addNodeStyles)
-  graphData.edges.forEach(addEdgeStyles)
-  Utils.processEdges(graphData.edges, { poly: 50 })
-  console.log(personData)
-  console.log(graphData)
+  // const getFilter = () => {
+  //   console.log('in getFilter()')
+  //   return filterState.nameFilter.length > 0
+  //     ? {
+  //         OR: [
+  //           { name_CONTAINS: filterState.nameFilter },
+  //           { surname_CONTAINS: filterState.nameFilter },
+  //         ],
+  //       }
+  //     : {}
+  // }
 
-  const handleFilterChange = (filterName, graphDataName) => (event) => {
+  //graphql cyphers
+  // var name = 'Amanda',
+  //   surname = 'Alexander'
+
+  // console.log('getting personData')
+  // const {
+  //   loading: loadingPerson,
+  //   data: personData,
+  //   error: errorPerson,
+  // } = useQuery(GET_PERSON, { variables: { filter: getFilter() } })
+
+  // if (!loadingPerson && !errorPerson && personData && personData.people[0]) {
+  //   var currentPerson = (({ name, surname }) => ({ name, surname }))(
+  //     personData.people[0]
+  //   )
+  //   name = currentPerson.name
+  //   surname = currentPerson.surname
+  //   // { name, surname } = {...dataPerson.people[0]}
+  // }
+
+  const handleFilterChange = (event) => {
     const val = event.target.value
     console.log(val)
 
-    setFilterState((oldFilterState) => ({
-      ...oldFilterState,
-      [filterName]: val,
-    }))
-    console.log('changed filter state')
+    // setFilterState((oldFilterState) => ({
+    //   ...oldFilterState,
+    //   [filterName]: val,
+    // }))
+    const nameArr = val.split(' ', 2)
+    console.log(nameArr)
+    getGraph({ variables: { nameInput: nameArr[0], surnameInput: nameArr[1] } })
+    console.log(data)
+
+    //manipulate json data for Graphin requirements
+    let graphData
+    if (data) {
+      graphData = JSON.parse(data.response)
+      graphData.nodes.forEach(addNodeStyles)
+      graphData.edges.forEach(addEdgeStyles)
+      Utils.processEdges(graphData.edges, { poly: 50 })
+    } else {
+      graphData = Utils.mock(13).circle().graphin()
+    }
+    console.log(graphData)
     setGraphState((oldGraphState) => ({
       ...oldGraphState,
-      [graphDataName]: graphData,
+      graphStateData: graphData,
     }))
-    console.log('changed graph state')
   }
+
+  // useEffect(() => {
+  //   // if (data) {
+  //   //manipulate json data for Graphin requirements
+  //   let graphData = JSON.parse(data.response)
+  //   graphData.nodes.forEach(addNodeStyles)
+  //   graphData.edges.forEach(addEdgeStyles)
+  //   Utils.processEdges(graphData.edges, { poly: 50 })
+  //   console.log(graphData)
+
+  //   setGraphState((oldGraphState) => ({
+  //     ...oldGraphState,
+  //     graphStateData: graphData,
+  //   }))
+  //   console.log('changed graph state')
+  //   // }
+  // }, [data])
 
   const checkEnter = (event) => {
     if (event.keyCode == 13) {
       console.log('enter')
-      handleFilterChange('nameFilter', 'graphStateData')(event)
+      handleFilterChange(event)
     }
   }
 
@@ -144,7 +164,7 @@ function GraphDisplay(props) {
             label="Search for a person"
             margin="normal"
             variant="outlined"
-            value={filterState.nameFilter}
+            // value={filterState.nameFilter}
             onKeyUp={checkEnter}
             InputProps={{
               ...params.InputProps,
