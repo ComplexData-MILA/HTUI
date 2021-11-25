0106c529af14a16adec4a284a02aef39fb7a9230
Ubuntu <ubuntu@amanda-dev.arbutus>
2021-11-25 20:33:51 +0000

    [Jacob] Handling autocomplete text manually

--- web-react/src/components/Search.js
+++ web-react/src/components/Search.js
@@ -1,69 +1,56 @@
 import React, { useState, useEffect } from 'react'
-import { useQuery, gql } from '@apollo/client'
 import '@antv/graphin/dist/index.css' // may be removed in the future by antv
 import {
-  TextField,
+    TextField,
 } from '@material-ui/core'
 import Autocomplete from '@material-ui/lab/Autocomplete'
+import { useQuery } from "react-query";
 
 
 export default function SearchBar(props) {
-  // declare useState hooks
-  const { classes, callback, apiHost } = props
-  const [people, setPeople] = useState([])
-  const [error, setError] = useState(null);
-  const [isLoaded, setIsLoaded] = useState(false);
+    const { classes, callback, apiHost } = props
+    const [textInput, setTextInput] = useState('');
 
-  // temporarily hardcode
-  // const people = [{name: "Harold", surname: "Oliver", id: 1162}];
-  // const fetchPeople = () => {
-  //   fetch(`${API_HOST}/allpeople`)
-  //   .then(response => response.json())
-  // }
-  // const people = fetchPeople()
-  // console.log(people)
-  // use hook with setPeople in .then, useEffect
-  useEffect(() => {
-    fetch(`${apiHost}/allpeople`)
-        .then(response => response.json())
-        .then(
-          data => { setIsLoaded(true); setPeople(data); console.log(data) },
-          error => {
-            setIsLoaded(true);
-            setError(error);
-          }
-        );
-  }, []);
+    const handleTextInputChange = event => {
+        setTextInput(event.target.value);
+    };
 
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  } else if (!isLoaded) {
-    return <div>Loading...</div>;
-  }
-  return (
-    <Autocomplete
-        // options={people.data.people}
-        options={people}
-        getOptionLabel={(option) => option.value}
-        onChange={callback} //{(event, value) => addSeedNode(value.id)}
-        disableClearable
-        renderInput={(params) => (
-          <TextField
-            id="search"
-            className={classes.textField}
-            {...params}
-            label="Search for a person"
-            margin="normal"
-            variant="outlined"
-            InputProps={{
-              ...params.InputProps,
-              type: 'search',
-              className: classes.input,
-            }}
-          />
-        )}
-      />
-  )
+    const { isLoading, error, data, isFetching } = useQuery(["search", textInput], () =>
+        fetch(`${apiHost}/allpeople?q=${textInput}`).then((res) => res.json())
+    );
+
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    }
+
+    // TODO [maybe]: Handle state in autocomplete instead of textfield
+    // https://stackoverflow.com/questions/63295924/a-component-is-changing-an-uncontrolled-autocomplete-to-be-controlled
+    return (
+        <Autocomplete
+            options={isLoading ? [] : data}
+            getOptionLabel={(option) => option.value}
+            onChange={callback}
+            noOptionsText={isLoading ? 'Loading...' : 'No options'}
+            // value={textInput}
+            // onInputChange={handleTextInputChange}
+            disableClearable
+            renderInput={(params) => (
+                <TextField
+                    id="search"
+                    className={classes.textField}
+                    value= {textInput}
+                    onChange= {handleTextInputChange}
+                    label="Search for an entity"
+                    margin="normal"
+                    variant="outlined"
+                    InputProps={{
+                        ...params.InputProps,
+                        type: 'search',
+                        className: classes.input,
+                    }}
+                    {...params}
+                />
+            )}
+        />
+    )
 }
-
-// export default withStyles(styles)(GraphDisplay)
